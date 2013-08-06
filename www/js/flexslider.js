56afe242a46e2094c3f83ab7d71e8df4b2616f5c
Matveev, Alexey <alexey.matveev@external.telekom.de>
2013-08-06 19:18:16 +0400

    upd

---
+++ www/js/flexslider.js
@@ -0,0 +1,16 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: amatveev
+ * Date: 06.08.13
+ * Time: 18:24
+ * To change this template use File | Settings | File Templates.
+ */
+$(window).load(function() {
+   $('.flexslider').flexslider({
+       animation: "slide",
+//       animationLoop: false
+//       itemWidth: 500
+//       itemMargin: 15,
+//       controlNav: "thumbnails"
+   });
+});
