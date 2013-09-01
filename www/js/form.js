26f5290b1e232db56f4aca0420d03dfca9545a86
david <david.lucky.star@gmail.com>
2013-09-01 16:51:13 -0700

    upd

--- www/js/form.js
+++ www/js/form.js
@@ -1,5 +1,18 @@
 $(function() {
     $('#personNumber').mask("9");
+    $('#personNumber').parent().find('div.clickers a.arrow-up').click(function(e) {
+        e.preventDefault();
+        var number = $('#personNumber').val();
+        if (number == 'undefined' || number == 9) return;
+        $('#personNumber').val(parseInt(number)+1);
+    });
+    $('#personNumber').parent().find('div.clickers a.arrow-down').click(function(e) {
+        e.preventDefault();
+        var number = $('#personNumber').val();
+        if (number == 'undefined' || number == 1) return;
+        $('#personNumber').val(number-1);
+    });
+
     $('#phoneNumber').mask("+7 (999) - 999 - 99 - 99");
 
     $('#desiredDate').attachDatepicker({
